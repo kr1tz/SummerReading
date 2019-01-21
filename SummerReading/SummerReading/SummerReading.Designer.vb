f2f10fe72d1aa5dac939ae8730ad3a513d3814cd
Kris Trinidad <kristrinidad@gmail.com>
2019-01-21 15:51:07 -0600

    Initial commit

---
+++ SummerReading/SummerReading/SummerReading.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PointsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox = New System.Windows.Forms.GroupBox()
+        Me.NumBooksTextBox = New System.Windows.Forms.TextBox()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.NumBooksLabel = New System.Windows.Forms.Label()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.BonusPointsLabel = New System.Windows.Forms.Label()
+        Me.BonusTextBox = New System.Windows.Forms.TextBox()
+        Me.FontDialog = New System.Windows.Forms.FontDialog()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.MenuStrip.SuspendLayout()
+        Me.GroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(571, 24)
+        Me.MenuStrip.TabIndex = 0
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PointsToolStripMenuItem, Me.SummaryToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'PointsToolStripMenuItem
+        '
+        Me.PointsToolStripMenuItem.Name = "PointsToolStripMenuItem"
+        Me.PointsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PointsToolStripMenuItem.Text = "&Points"
+        '
+        'SummaryToolStripMenuItem
+        '
+        Me.SummaryToolStripMenuItem.Name = "SummaryToolStripMenuItem"
+        Me.SummaryToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SummaryToolStripMenuItem.Text = "&Summary"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ToolStripSeparator1, Me.FontToolStripMenuItem, Me.ColorToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ClearToolStripMenuItem.Text = "C&lear"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FontToolStripMenuItem.Text = "&Font"
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ColorToolStripMenuItem.Text = "&Color"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'GroupBox
+        '
+        Me.GroupBox.Controls.Add(Me.NumBooksTextBox)
+        Me.GroupBox.Controls.Add(Me.NameTextBox)
+        Me.GroupBox.Controls.Add(Me.NumBooksLabel)
+        Me.GroupBox.Controls.Add(Me.NameLabel)
+        Me.GroupBox.Location = New System.Drawing.Point(12, 44)
+        Me.GroupBox.Name = "GroupBox"
+        Me.GroupBox.Size = New System.Drawing.Size(369, 100)
+        Me.GroupBox.TabIndex = 1
+        Me.GroupBox.TabStop = False
+        '
+        'NumBooksTextBox
+        '
+        Me.NumBooksTextBox.Location = New System.Drawing.Point(144, 57)
+        Me.NumBooksTextBox.Name = "NumBooksTextBox"
+        Me.NumBooksTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.NumBooksTextBox.TabIndex = 3
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Location = New System.Drawing.Point(144, 20)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(219, 20)
+        Me.NameTextBox.TabIndex = 2
+        '
+        'NumBooksLabel
+        '
+        Me.NumBooksLabel.AutoSize = True
+        Me.NumBooksLabel.Location = New System.Drawing.Point(46, 60)
+        Me.NumBooksLabel.Name = "NumBooksLabel"
+        Me.NumBooksLabel.Size = New System.Drawing.Size(92, 13)
+        Me.NumBooksLabel.TabIndex = 1
+        Me.NumBooksLabel.Text = "Number of &Books:"
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(100, 23)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(38, 13)
+        Me.NameLabel.TabIndex = 0
+        Me.NameLabel.Text = "&Name:"
+        '
+        'BonusPointsLabel
+        '
+        Me.BonusPointsLabel.AutoSize = True
+        Me.BonusPointsLabel.Location = New System.Drawing.Point(78, 190)
+        Me.BonusPointsLabel.Name = "BonusPointsLabel"
+        Me.BonusPointsLabel.Size = New System.Drawing.Size(109, 13)
+        Me.BonusPointsLabel.TabIndex = 2
+        Me.BonusPointsLabel.Text = "Bonus Points Earned:"
+        '
+        'BonusTextBox
+        '
+        Me.BonusTextBox.Location = New System.Drawing.Point(193, 187)
+        Me.BonusTextBox.Name = "BonusTextBox"
+        Me.BonusTextBox.ReadOnly = True
+        Me.BonusTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.BonusTextBox.TabIndex = 3
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(571, 270)
+        Me.Controls.Add(Me.BonusTextBox)
+        Me.Controls.Add(Me.BonusPointsLabel)
+        Me.Controls.Add(Me.GroupBox)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "MainForm"
+        Me.Text = "Summer Reading Program"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.GroupBox.ResumeLayout(False)
+        Me.GroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PointsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SummaryToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents FontToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GroupBox As System.Windows.Forms.GroupBox
+    Friend WithEvents NumBooksTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents NameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents NumBooksLabel As System.Windows.Forms.Label
+    Friend WithEvents NameLabel As System.Windows.Forms.Label
+    Friend WithEvents BonusPointsLabel As System.Windows.Forms.Label
+    Friend WithEvents BonusTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents FontDialog As System.Windows.Forms.FontDialog
+    Friend WithEvents ColorDialog As System.Windows.Forms.ColorDialog
+
+End Class
---
+++ SummerReading/SummerReading/SummerReading.vb
@@ -0,0 +1,145 @@
+﻿Public Class MainForm
+
+    Private TotalReadersInteger As Integer
+    Private TotalBooksInteger As Integer
+
+    Private Function BonusFunction(ByVal NumOfBooksInteger As Integer) As Integer
+        'Calculating the bonus points for the books.
+
+        Select Case NumOfBooksInteger
+            Case 1 To 3
+                BonusFunction = 10 * NumOfBooksInteger
+            Case 4 To 6
+                BonusFunction = 30 + 15 * (NumOfBooksInteger - 3)
+            Case Is >= 7
+                BonusFunction = 75 + 20 * (NumOfBooksInteger - 6)
+        End Select
+
+
+    End Function
+
+
+
+    
+
+    Private Sub PointsToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles PointsToolStripMenuItem.Click
+
+        Dim NumOfBooksInteger As Integer
+        Dim MessageString As String
+
+
+
+        Try
+            NumOfBooksInteger = Integer.Parse(NumBooksTextBox.Text)
+
+            If NumOfBooksInteger > 0 Then
+                'Calculate the bonus
+                BonusTextBox.Text = BonusFunction(NumOfBooksInteger).ToString()
+
+                TotalBooksInteger += NumOfBooksInteger
+                TotalReadersInteger += 1
+            Else
+                MessageString = "The number of books must be greater than zero."
+                MessageBox.Show(MessageString, "Input Error", MessageBoxButtons.OK)
+
+                With NumBooksTextBox
+                    .SelectAll()
+                    .Focus()
+                End With
+
+            End If
+
+        Catch BooksException As FormatException
+            MessageString = "You must input a number for the number of books read."
+            MessageBox.Show(MessageString, "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+            With NumBooksTextBox
+                .SelectAll()
+                .Focus()
+            End With
+
+        Catch AnException As Exception
+            MessageBox.Show("Error: " & AnException.Message)
+        End Try
+
+
+
+
+
+
+    End Sub
+
+    Private Sub SummaryToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles SummaryToolStripMenuItem.Click
+
+        'Find the average number of books read
+        Dim MessageString As String
+        Dim AvgBooksDecimal As Decimal
+
+        If TotalBooksInteger > 0 Then
+            AvgBooksDecimal = Convert.ToDecimal(TotalBooksInteger / TotalReadersInteger)
+            MessageString = "Average Number of Books Read: " & AvgBooksDecimal.ToString("N2")
+            MessageBox.Show(MessageString, "Summary", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+
+            MessageString = "No data to summarize"
+            MessageBox.Show(MessageString, "No Data", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        End If
+
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+
+        Me.Close()
+
+    End Sub
+
+
+
+    Private Sub ClearToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ClearToolStripMenuItem.Click
+
+        NameTextBox.Clear()
+        NumBooksTextBox.Clear()
+        BonusTextBox.Clear()
+
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles FontToolStripMenuItem.Click
+
+        With FontDialog
+            .ShowDialog()
+            NameTextBox.Font = .Font
+        End With
+
+        With FontDialog
+            .ShowDialog()
+            NumBooksTextBox.Font = .Font
+        End With
+
+
+
+
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ColorToolStripMenuItem.Click
+
+        With ColorDialog
+            .ShowDialog()
+            NameTextBox.ForeColor = .Color
+        End With
+
+        With ColorDialog
+            .ShowDialog()
+            NumBooksTextBox.ForeColor = .Color
+        End With
+
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+
+        MessageBox.Show("Programmed By: Kris Trinidad", "Summer Reading Program", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+End Class
